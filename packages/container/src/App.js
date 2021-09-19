48aaaaf43664204e8c09b7662e75b9a06be83592
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-09-18 21:38:50 -0500

    feat: format code

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {mount} from 'marketing/MarketingApp';
+import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
     <h1>
-   hello there 
-  </h1>
-  <hr />
-  <MarketingApp />
+      hello there
+    </h1>
+    <hr />
+    <MarketingApp />
   </div>
 }
