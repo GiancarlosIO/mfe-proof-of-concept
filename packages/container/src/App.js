e8e82112dc3087eaf167fdfa97d270f7fe644b0e
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-09-21 19:50:09 -0500

    fix: add css-in-jss className prefix to dont get css classname colissions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import * as React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 export default () => {
-  return <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-  </BrowserRouter>
+  return <StylesProvider generateClassName={generateClassName}>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  </StylesProvider>
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,16 +5,21 @@ import {
   BrowserRouter
 } from 'react-router-dom'
 import {
-  StylesProvider
+  StylesProvider,
+  createGenerateClassName,
 } from '@material-ui/core/styles';
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+})
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
