f585abf379feb7005b3c3589cbd4376fada26e0f
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-09-21 23:03:43 -0500

    feat: setup multi tier navigation for the microfrontens

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,21 +1,38 @@
 import React from 'react'
-
+import {createMemoryHistory, createBrowserHistory} from 'history'
 import ReactDOM from 'react-dom'
 
 import App from './App'
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaulHistory }) => {
+  const history = defaulHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate)
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
   )
+
+  return {
+    onParentNavigate({ pathname: nextPathname}) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname ) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 
 // for development
 if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#_marketing-dev-root')
   if (el) {
-    mount(el)
+    mount(el, {
+      defaulHistory: createBrowserHistory(),
+    })
   }
 }
 
